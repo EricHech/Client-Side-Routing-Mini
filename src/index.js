d60cb0b467949fe3b7c2296ea319b7198ac9fc51
Teleson <Teleson@Courtneys-MacBook-Pro.local>
2018-02-14 14:14:47 -0500

    Some editing. Unfinished.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import 'bootstrap/dist/css/bootstrap.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+function About() {
+	return (
+		<div>
+			<h1>About Coponent</h1>
+			<p id='paragraph'>This is the About Component</p>
+		</div>
+	);
+}
+
+function Home() {
+	return (
+		<div>
+			<h1>Home Coponent</h1>
+			<p id='paragraph'>This is the Home Component</p>
+		</div>
+	);
+}
+
+ReactDOM.render(
+	<Router>
+		<div>
+			<Home />
+			<About />
+		</div>
+	</Router>,
+	document.getElementById('root')
+);
