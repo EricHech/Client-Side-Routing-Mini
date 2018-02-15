d9e483adcd96e8e296f32481d70d8b0ced0bc0b9
Eric <erichech@gmail.com>
2018-02-14 19:58:34 -0500

    Updating mini to match Luis' code.

--- src/index.js
+++ src/index.js
@@ -1,31 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
 
-function About() {
-	return (
-		<div>
-			<h1>About Coponent</h1>
-			<p id='paragraph'>This is the About Component</p>
-		</div>
-	);
-}
+import './index.css';
+import App from './App';
 
-function Home() {
-	return (
-		<div>
-			<h1>Home Coponent</h1>
-			<p id='paragraph'>This is the Home Component</p>
-		</div>
-	);
-}
-
-ReactDOM.render(
-	<Router>
-		<div>
-			<Home />
-			<About />
-		</div>
-	</Router>,
-	document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
